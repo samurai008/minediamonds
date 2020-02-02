6d0cdec7eb93d75d29a48b7a41360682c15300b8
Nilabjo Sanyal <n.sanyal@hotmail.com>
2020-02-02 18:48:33 +0530

    added hint functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import diamond from './diamond.svg';
+import arrow from './arrow.svg';
 
 // Create 8 x 8 board
 // true - diamond , false - no diamond
@@ -8,8 +9,11 @@ import diamond from './diamond.svg';
 const SIZE_X = 8;
 const SIZE_Y = 8;
 
-let board = [];
+let board = {};
+let gameOver = false;
 let turnedSquares = 0;
+let totalDiamonds = 0;
+let arrowPathCache = {};
 const appEl = document.getElementById('app');
 const boardEl = document.createElement('div');
 const scoreboardEl = document.createElement('div');
@@ -20,6 +24,7 @@ appEl.appendChild(scoreboardEl);
 
 let squareEl;
 let rowEl;
+let randomBool;
 for (let i = 0; i < SIZE_X; i++) {
   rowEl = document.createElement('div');
   boardEl.appendChild(rowEl);
@@ -33,20 +38,109 @@ for (let i = 0; i < SIZE_X; i++) {
     squareEl.addEventListener('click', handleSquareClick);
     rowEl.appendChild(squareEl);
     // will there be a diamond
-    board[i][j] = Math.random() >= 0.5;
+    randomBool = Math.random() >= 0.5;
+    board[i][j] = { diamond: randomBool, clicked: false };
+    if (randomBool) {
+      totalDiamonds = totalDiamonds + 1;
+    }
   }
 }
 
+// implement gameover
+
 scoreboardEl.innerHTML = (SIZE_X * SIZE_Y) - turnedSquares;
+
+function dfs(row, col, paths = []) {
+  let S = [];
+  let Visited = [];
+  S.push({ row, col, visited: false });
+  let findNearest = false;
+  let pointerDeg;
+
+  while (S.length > 0) {
+    let v = S.pop();
+    let r = v.row;
+    let c = v.col;
+    if (Visited.filter(v => v === board[r][c]).length === 0) {
+      Visited.push(board[r][c]);
+
+      if (board[r][c].diamond && !board[r][c].clicked && !findNearest) {
+        if (row > r && col === c) {
+          // north
+          pointerDeg = 0;
+        } else if (row < r && col === c) {
+          // south
+          pointerDeg = 180;
+        } else if (row > r && col > c) {
+          // north west
+          pointerDeg = 315;
+        } else if (row < r && col > c) {
+          // south west
+          pointerDeg = 225;
+        } else if (row < r && col < c) {
+          // south east
+          pointerDeg = 135;
+        } else if (row > r && col < c) {
+          // north east
+          pointerDeg = 45;
+        } else if (row === r && col < c) {
+          // east
+          pointerDeg = 90;
+        } else {
+          // west
+          pointerDeg = 270;
+        }
+
+        findNearest = true;
+        break;
+      }
+
+      // up
+      if (board[r - 1] && Visited.filter(v => v === board[r - 1][c]).length === 0) {
+        S.push({ row: r - 1, col: c});
+      }
+
+      // down
+      if (board[r + 1] && Visited.filter(v => v === board[r + 1][c]).length === 0) {
+        S.push({ row: r + 1, col: c });
+      }
+
+      // left
+      if (board[r][c - 1] && Visited.filter(v => v === board[r][c - 1]).length === 0) {
+        S.push({ row: r, col: c - 1 });
+      }
+
+      // right
+      if (board[r][c + 1] && Visited.filter(v => v === board[r][c + 1]).length === 0) {
+        S.push({ row: r, col: c + 1 });
+      }
+    }
+  }
+
+  return pointerDeg;
+}
 
 function handleSquareClick(e) {
   const el = e.target;
-  const row = el.getAttribute('data-row');
-  const col = el.getAttribute('data-col')
-  if (board[row][col]) {
-    el.innerHTML = '<img src="' + diamond + '" class="diamond" />'
-  } else {
+  const row = parseInt(el.getAttribute('data-row'));
+  const col = parseInt(el.getAttribute('data-col'));
+  console.log(row, col);
+  board[row][col].clicked = true;
+  if (board[row][col].diamond) {
+    // el.innerHTML = '<img src="' + diamond + '" class="diamond" />';
+    el.style.backgroundImage = 'url(' + diamond + ')';
+    el.style.backgroundSize = '70%';
+    el.style.backgroundRepeat = 'no-repeat';
+    el.style.backgroundPosition = 'center';
     el.innerHTML = '';
+    totalDiamonds = totalDiamonds - 1;
+  } else {
+    // show hint
+    el.innerHTML = '<img src="' + arrow + '" style="width: 70%; transform: rotate(' + dfs(row, col) + 'deg" />';
+    setTimeout(function () {
+      el.removeChild(el.childNodes[0]);
+      el.innerHTML = '?';
+    }, 800);
   }
 
   // update the score
